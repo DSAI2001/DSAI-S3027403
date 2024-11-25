9e7d303c092cf4d78585849ceaca070bb32b723c
DSAI2001 <saicharan9874@gmail.com>
2024-11-25 15:05:11 +0000

    commit 02

--- app/src/main/java/com/dorapallysai/eventbooking/UserData.kt
+++
@@ -1,8 +0,0 @@
-package com.dorapallysai.eventbooking
-
-data class UserData(
-    val name: String = "",
-    val gender: String = "",
-    val email: String = "",
-    val password: String = ""
-)
